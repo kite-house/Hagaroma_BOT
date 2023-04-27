0d91cdd6423cfaeefc8ebd54ec727b4b239e6b9b
Kite-house <hodosok1301@gmail.com>
2023-04-28 01:55:33 +0700

    Version0.7(Beta)

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 const mysql = require('mysql2')
 
 module.exports = mysql.createConnection({ 
-    host: 'sql.freedb.tech',
-    user: 'freedb_Admin_BOT',
-    database: 'freedb_HAGAROMA-BOT',
-    password: 'PMzBg4Snz3!3hF2'
+    host: 'db4free.net',
+    user: 'admin_bot',
+    database: 'hagaroma_bot',
+    password: 'PMzBg4Snz3!3hF2',
 })
 
 module.exports.help = {
     name : "db",
     descrtiption: 'db'
 }
-
-
